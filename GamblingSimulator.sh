c6996069184e942ddcb070968284b35ae0feda1f
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-09 00:27:52 +0530

    Added a decider to know loss or win

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,6 +1,17 @@
-#Welcome to Gambling Simulator Problem
+#!/bin/bash -x
+#Welcom to Gambling Simulator Problem
 
-	stake=100
-	bet=1
+	stakeEveryDay=100
+	betEveryGame=1
 
+	Decider=$((RANDOM%2))  #generates a random value 1 or 0 
+	won=1;
+	lost=0;
 
+#if condition decides winner or looser by comparing random values with win or lost variables
+	if [ $Decider -eq $lost ]; 
+	then
+		echo "Lost"
+	else
+		echo "Won"
+	fi
