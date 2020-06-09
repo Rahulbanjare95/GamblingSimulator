23087378fbda2c9ce34a62ef188c8dce95e82169
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-09 13:25:14 +0530

    made a function to calculate final amount win or lose after 20 days

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 #-------UC 1------
 
-	stakeEveryDay=100
+	stake=100
 	betEveryGame=1
 
 #-------UC 2-------
@@ -9,24 +9,23 @@
 	Decider=$((RANDOM%2))  #generates a random value 1 or 0
 	won=1;
 	lost=0;
-#if condition decides winner or looser by comparing random values with win or lost
-	if [ $Decider -eq $lost ]
-	then
-		echo "Lost"
-	else
-		echo "Won"
-	fi
+
 
 #--------UC 3
 	limitPercentage=50
-	winningAmountforResign=$(($stakeEveryDay*$limitPercentage/100+$stakeEveryDay));
-	echo "	Maximum amount to resign = $winningAmountforResign"
-	losingAmountforResign=$(($stakeEveryDay-$stakeEveryDay*$limitPercentage/100));
-	echo "	Minimum amount to resign = $losingAmountforResign"
+   limitValue=$(($limitPercentage*$stake/100))
 
-	while [[ $stakeEveryDay -le $winningAmountforResign && $stakeEveryDay -ge $losingAmountforResign ]]
+   winningAmountforResign=$(($stake*$limitPercentage/100+$stake));
+   echo "   Maximum amount to resign = $winningAmountforResign"
+   losingAmountforResign=$(($stake-$stake*$limitPercentage/100));
+   echo "   Minimum amount to resign = $losingAmountforResign"
+
+
+
+function stakecalculator(){
+	stakeEveryDay=$stake
+	while [[ $stakeEveryDay -lt $winningAmountforResign && $stakeEveryDay -gt $losingAmountforResign ]]
 	do
-
 	Decider=$((RANDOM%2))
 		if [ $Decider -eq $won ]
 		then
@@ -35,4 +34,25 @@
 			stakeEveryDay=$(( $stakeEveryDay-$betEveryGame ))
 		fi
 	done
-	echo "Limit Reached" $numberOfWinnifBet
+}
+
+#--------Uc 4
+	day=1
+	totalamount=0
+	totalDays=20
+
+	for (( day=1; day<$totalDays; day++ ))
+	do
+	stakecalculator
+		if [ $stakeEveryDay -eq $winningAmountforResign ]
+		then
+			totalamount=$(( totalamount+limitValue ))
+
+		else
+			totalamount=$(( totalamount-limitValue))
+		fi
+
+
+	done
+	echo "Amount final after 20 days"$totalamount
+
