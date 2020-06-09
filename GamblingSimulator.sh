a94cc96782c44a879ba3624ea76e802a04f906a9
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-09 16:07:55 +0530

    Refactored and Made all variables follow Naming Coventions

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -1,63 +1,50 @@
-#!/bin/bash
-#-------UC 1------
-
-	stake=100
-	betEveryGame=1
-
-#-------UC 2-------
-
-	Decider=$((RANDOM%2))  #generates a random value 1 or 0
-	won=1;
-	lost=0;
 
 
-#--------UC 3
-	limitPercentage=50
-   limitValue=$(($limitPercentage*$stake/100))
-
-   winningAmountforResign=$(($stake*$limitPercentage/100+$stake));
+	read -p "Enter the Stake " STAKE
+	read -p "Enter the bet for everygame" BET_EVERY_GAME
+	WON=1;
+	read -p "Enter the % limt of win or loss" LIMIT_PERCENTAGE
+	LIMIT_PERCENTAGE=50
+   LIMIT_VALUE=$(($LIMIT_PERCENTAGE*$STAKE/100))
+   winningAmountforResign=$(($STAKE*$LIMIT_PERCENTAGE/100+$STAKE));
    echo "   Maximum amount to resign = $winningAmountforResign"
-   losingAmountforResign=$(($stake-$stake*$limitPercentage/100));
+   losingAmountforResign=$(($STAKE-$STAKE*$LIMIT_PERCENTAGE/100));
    echo "   Minimum amount to resign = $losingAmountforResign"
 
-
-
-function stakecalculator(){
-	stakeEveryDay=$stake
+	function stakecalculator(){
+	stakeEveryDay=$STAKE
 	while [[ $stakeEveryDay -lt $winningAmountforResign && $stakeEveryDay -gt $losingAmountforResign ]]
 	do
 	Decider=$((RANDOM%2))
-		if [ $Decider -eq $won ]
+		if [ $Decider -eq $WON ]
 		then
-			stakeEveryDay=$(( $stakeEveryDay+$betEveryGame ))
+			stakeEveryDay=$(( $stakeEveryDay+$BET_EVERY_GAME ))
 		else
-			stakeEveryDay=$(( $stakeEveryDay-$betEveryGame ))
+			stakeEveryDay=$(( $stakeEveryDay-$BET_EVERY_GAME ))
 		fi
 	done
-}
-
+	}
 	declare -A resultsDayWise
-	totalamount=0
-	totalDays=20
-	wins=0
-	losses=0
-	for (( day=0; day<$totalDays; day++ ))
+	TOTAL_AMOUNT=0
+	TOTAL_DAYS=20
+	WINS=0
+	LOSSES=0
+	for (( day=0; day<$TOTAL_DAYS; day++ ))
 	do
 	stakecalculator
 		if [ $stakeEveryDay -eq $winningAmountforResign ]
 		then
-			totalamount=$(( totalamount+limitValue ))
-			resultsDayWise["Day $day"]=$totalamount
-			(( wins++ ))
+			TOTAL_AMOUNT=$(( TOTAL_AMOUNT+LIMIT_VALUE ))
+			resultsDayWise["Day $day"]=$TOTAL_AMOUNT
+			(( WINS++ ))
 		else
-			totalamount=$(( totalamount-limitValue))
-			resultsDayWise["Day $day"]=$totalamount
-			(( losses++ ))
+			TOTAL_AMOUNT=$(( TOTAL_AMOUNT-LIMIT_VALUE ))
+			resultsDayWise["Day $day"]=$TOTAL_AMOUNT
+			(( LOSSES++ ))
 		fi
 	done
-	echo "No of days won = $wins  by $((wins*limitValue)) "
-	echo "No of days lost are $losses by  $((losses*limitValue))"
-
+	echo "No of days won = $WINS  by $((WINS*LIMIT_VALUE)) "
+	echo "No of days lost are $LOSSES by  $((LOSSES*LIMIT_VALUE))"
 
 	luckyDayValue=$( printf "%s\n" ${resultsDayWise[@]} | sort -nr | head -1 )
 	luckyDayKey=$( printf "%s\n" ${!resultsDayWise[@]} | sort -nr | head -1 )
@@ -68,7 +55,7 @@ function stakecalculator(){
 	echo "Lucky Day is $luckyDayKey $luckyDayValue "
 	echo "UnLucky Day is $unluckyDayKey $unluckyDayValue "
 
- 	if [ $totalamount -gt 0 ]
+ 	if [ $TOTAL_AMOUNT -gt 0 ]
 	then
 		echo " You can play "
 	else
