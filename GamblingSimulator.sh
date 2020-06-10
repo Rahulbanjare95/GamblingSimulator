e1433a8eda31ffe091a9bca893b5a22d0a8e284e
Rahulbanjare95 <rahul.banjare95@gmail.com>
2020-06-10 20:40:30 +0530

    Added multiple Months Calculator

--- GamblingSimulator.sh
+++ GamblingSimulator.sh
@@ -2,7 +2,7 @@
 	read -p "Enter the bet for everygame" BET_EVERY_GAME
 	WON=1;
 	read -p "Enter the % limt of win or loss" LIMIT_PERCENTAGE
-
+	read -p "Enter the number of days you want to play" TOTAL_DAYS
    LIMIT_VALUE=$(($LIMIT_PERCENTAGE*$STAKE/100))
    winningAmountforResign=$(($STAKE*$LIMIT_PERCENTAGE/100+$STAKE));
    echo "   Maximum amount to resign = $winningAmountforResign"
@@ -22,11 +22,14 @@
 		fi
 	done
 	}
+
 	declare -A resultsDayWise
+
+
 	TOTAL_AMOUNT=0
-	TOTAL_DAYS=20
 	WINS=0
 	LOSSES=0
+	function monthlyCalculator(){
 	for (( day=0; day<$TOTAL_DAYS; day++ ))
 	do
 	stakecalculator
@@ -47,6 +50,7 @@
 	echo "No of days won = $WINS  by $((WINS*LIMIT_VALUE)) "
 	echo "No of days lost are $LOSSES by  $((LOSSES*LIMIT_VALUE))"
 
+
 	luckyDayValue=$( printf "%s\n" ${resultsDayWise[@]} | sort -nr | head -1 )
 	luckyDayKey=$( printf "%s\n" ${!resultsDayWise[@]} | sort -nr | head -1 )
 
@@ -55,10 +59,13 @@
 
 	echo "Lucky Day is $luckyDayKey $luckyDayValue "
 	echo "UnLucky Day is $unluckyDayKey $unluckyDayValue "
-
- 	if [ $TOTAL_AMOUNT -gt 0 ]
-	then
-		echo " You can play  $TOTAL_AMOUNT"
-	else
-		echo " You are in loss $TOTAL_AMOUNT"
-	fi
+	}
+	monthlyCalculator
+	monthsPlayed=1
+	while [ $TOTAL_AMOUNT -gt 0 ]
+	do
+	echo "Yes You Can Play For Next month"
+	monthlyCalculator
+	(( monthsPlayed++ ))
+	done
+	echo "Total months played $monthsPlayed"
